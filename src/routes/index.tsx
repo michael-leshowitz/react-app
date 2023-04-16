ce87a9fc1b04fb309e4c71d04d75c7d7218f9f25
Michael <michael.leshowitz@gmail.com>
2023-04-16 01:16:06 +0000

    Updated index to use grid layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,32 @@
+import { Grid } from "@mui/material";
 import RecipeCard from "../components/recipeCard/recipeCard";
+import { IRecipeSnippit } from "../context/types";
 
-export default function Index() {
+// TODO: Change this to an api call
+const getTopRecipes = (): IRecipeSnippit[] => {
+  const recipe_snippits = [1, 2, 3, 4, 5, 6].map((mock) => {
     return (
-      <p id="zero-state">
-        This is a demo for React Router.
-        <br />
-        Check out{" "}
-        <a href="https://reactrouter.com">
-          the docs at reactrouter.com
-        </a>
-        {/* TODO: Create a grid of 6 Recipe cards for this page */}
-        <RecipeCard
-          recipe_snippit={{
-            name: "Test-cipe",
-            description: "This is how I describe it",
-            rating: 5,
-            tags: [],
-          }}
-        />
-        .
-      </p>
+      {
+          name: `Test-cipe ${mock}`,
+          description: "This is how I describe it",
+          rating: mock - 1,
+          tags: [],  
+      }
+    )
+  })  as IRecipeSnippit[];
+  return recipe_snippits;
+}
+export default function Index() {
+  const topSixRecipies = getTopRecipes();
+    return ( 
+        <Grid container spacing={12} columns={3}>
+          {topSixRecipies.map((recipe_snippit) => (
+            <Grid item xs={1}>
+              <RecipeCard
+              recipe_snippit={recipe_snippit}
+              />
+            </Grid>
+          ))}
+        </Grid>        
     );
   }
