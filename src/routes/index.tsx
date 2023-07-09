2ff5d300ba6ad9b8f97dc71784f41c77c592bade
Michael <michael.leshowitz@gmail.com>
2023-07-09 17:41:39 +0000

    Removed console log

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,29 +9,7 @@ export default function Index() {
   const [recipeSnippits, setRecipeSnippits] = useState<IRecipeSnippit[]>([]);
 
   const getTopRecipes = async(): Promise<IRecipeSnippit[]> => {
-    // const recipe_snippits = [1,2,3].map((mock) => {
-    //   return (
-    //     {
-    //         name: `Test-cipe ${mock}`,
-    //         description: "This is how I describe it",
-    //         rating: mock - 1,
-    //         tags: [],  
-    //     }
-    //   )
-    // })  as IRecipeSnippit[];
-    console.log("starting api request")
     const recipe_snippits = await getTopRecipesInRange(0,1);
-    console.log(recipe_snippits);
-    console.log("finished request")
-    // add a pad...
-    recipe_snippits.push(
-          {
-            name: `Test-cipe ${2}`,
-            description: "This is how I describe it",
-            rating: 2 - 1,
-            tags: [],  
-        }
-    );
     return recipe_snippits;
   }
 
@@ -45,6 +23,7 @@ export default function Index() {
   
     return ( 
         (!recipeSnippits.length?
+          // TODO: Consider some loading indicator
           null
           :<RecipeCardGrid RecipeSnippits={recipeSnippits} />
         )
