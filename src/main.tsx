bbfa84609cf9f6418a015a02aa5f10d29da469cd
Michael <michael.leshowitz@gmail.com>
2023-09-26 00:55:50 +0000

    cleaned up some tutorial leftoverse

--- src/main.tsx
+++ src/main.tsx
@@ -8,10 +8,7 @@ import {
   Route
 } from 'react-router-dom'
 import Root, { loader as routLoader, action as rootAction } from './routes/root'
-import EditContact, { action as editAction } from './routes/edit'
 import ErrorPage from './error-page'
-import Contact, { loader as contactLoader, action as contactAction } from './routes/contact'
-import { action as destroyAction } from './routes/destroy'
 import Index from './routes'
 import RecipeSearchResults from './routes/recipeSearchResult'
 
@@ -19,12 +16,9 @@ const JSXRouter = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<Root />} path='/' errorElement={<ErrorPage/>} action={rootAction} loader={routLoader}>
       <Route errorElement={<ErrorPage/>}>
-        <Route element={<Contact/>} path='contacts/:contactId' loader={contactLoader} action={contactAction} />
         <Route index={true} element={<Index />} />
         <Route element={<RecipeSearchResults />} path='recipe/search/:keyword'/>
         {/* In general, all routes should have their own loaders */}
-        <Route element={<EditContact/>} path='contacts/:contactId/edit' loader={contactLoader} action={editAction}/>
-        <Route element={<EditContact/>} path='contacts/:contactId/destroy' action={destroyAction} errorElement={<div>Oops there was an erro</div>}/>
       </Route>
     </Route>
   )
